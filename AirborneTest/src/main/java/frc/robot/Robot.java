def301e97d0380196e548ff18be35fafca86add6
FRCTeam1518Student1 <Raider.Robotics@palmaccsd.org>
2020-01-22 21:06:59 -0500

    Added gyro and SparkMax to toast a motor

--- AirborneTest/src/main/java/frc/robot/Robot.java
+++ AirborneTest/src/main/java/frc/robot/Robot.java
@@ -8,6 +8,9 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.Joystick;
 //import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -16,6 +19,8 @@ import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.components.LimeLight;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 /**
  * This is a demo program showing how to use Mecanum control with the RobotDrive
@@ -26,6 +31,7 @@ public class Robot extends TimedRobot {
   private static final int kRearLeftChannel = 8;
   private static final int kFrontRightChannel = 10;
   private static final int kRearRightChannel = 9;
+  private static final int kThrowerMotor = 4;
 
   private static final int kJoystickChannel = 0;
 
@@ -39,6 +45,10 @@ public class Robot extends TimedRobot {
   private static final double targetHeight = 18;  //Inches
   private static final double optimalDistance = 84; //Inches
 
+  public static Gyro rioGyro = new ADXRS450_Gyro();
+
+  private CANSparkMax throwerMotor1;
+
   @Override
   public void robotInit() {
 
@@ -47,6 +57,11 @@ public class Robot extends TimedRobot {
     WPI_TalonSRX rearLeft = new WPI_TalonSRX(kRearLeftChannel);
     WPI_TalonSRX frontRight = new WPI_TalonSRX(kFrontRightChannel);
     WPI_TalonSRX rearRight = new WPI_TalonSRX(kRearRightChannel);
+
+    throwerMotor1 = new com.revrobotics.CANSparkMax(kThrowerMotor, MotorType.kBrushless);
+
+    rioGyro.calibrate();
+    rioGyro.reset();
 
     // Invert the left side motors.
     // You may need to change or remove this to match your robot.
@@ -64,59 +79,63 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+
+    // Thrower Motor by the Throttle
+    throwerMotor1.set(m_stick.getThrottle());
+    SmartDashboard.putNumber("Thrower Throttle", m_stick.getThrottle());
     // Use button 11 to change drive mode (joystick or LimeLight)
     //btnDriveMode.whenPressed(new ChangeDrive());
 
     // Use the joystick X axis for lateral movement, Y axis for forward
     // movement, and Z axis for rotation.
-
-    if(DriveByStick){
+    SmartDashboard.putNumber("Gyro Angle", rioGyro.getAngle());
+    // if(DriveByStick){
       m_robotDrive.driveCartesian(Math.pow(m_stick.getX(), 3)*.75, Math.pow(-m_stick.getY(), 3)*1, Math.pow(m_stick.getZ(), 3)*.5, 0.0);
-    }
-    else{
-      Double horizontalOffset = lime.getTargetOffsetHorizontal();
-      Double verticalOffset = lime.getTargetOffsetVertical();
-      Double targetArea = lime.getTargetArea();
+    // }
+    // else{
+    //   Double horizontalOffset = lime.getTargetOffsetHorizontal();
+    //   Double verticalOffset = lime.getTargetOffsetVertical();
+    //   Double targetArea = lime.getTargetArea();
   
-      Double minPowerFloor = 0.25;
-      Double newX = 0.0;
-      Double newY = 0.0;
-      Double newZ = 0.0;
-      Double targetDistance = optimalDistance;
+    //   Double minPowerFloor = 0.25;
+    //   Double newX = 0.0;
+    //   Double newY = 0.0;
+    //   Double newZ = 0.0;
+    //   Double targetDistance = optimalDistance;
 
-      // Compute the left or right power to turn the robot to face the target
-      if (Math.abs(horizontalOffset) > 8.0) {
-        // newZ = minPowerFloor * (int)Math.signum(horizontalOffset);
-        newZ = minPowerFloor* (int)Math.signum(horizontalOffset) + Math.sin(horizontalOffset*Math.PI/180);
-      }
-      else {
-        newZ = 0.0;
-      }
+    //   // Compute the left or right power to turn the robot to face the target
+    //   if (Math.abs(horizontalOffset) > 8.0) {
+    //     // newZ = minPowerFloor * (int)Math.signum(horizontalOffset);
+    //     newZ = minPowerFloor* (int)Math.signum(horizontalOffset) + Math.sin(horizontalOffset*Math.PI/180);
+    //   }
+    //   else {
+    //     newZ = 0.0;
+    //   }
 
-      // Compute the distance to the target based on height and power to move forward/backward
-      if(Math.abs(verticalOffset) > 0){
-      targetDistance = targetHeight / Math.tan(verticalOffset*Math.PI/180);
-      }
-      else{targetDistance = optimalDistance;}
-      SmartDashboard.putNumber("targetDistance", targetDistance);
-      if(Math.abs(optimalDistance - targetDistance) >= 6 ){
-        if(targetDistance > optimalDistance) {
-          newX = minPowerFloor;
-        }
-        else if(targetDistance < optimalDistance){
-          newX = -minPowerFloor;
-        }
-      }
-      else {newX = 0.0;}
-      SmartDashboard.putNumber("newX", newX);
-      //Distance tracking by target area - decided too likely to be wrong
-      // if (targetArea < 0.45) {
-      //   newY = minPowerFloor*1.15;
-      // } else if (targetArea > 0.65) {
-      //   newY = -minPowerFloor*1.15;
-      // }
-      m_robotDrive.driveCartesian(newY, newX, newZ, 0.0);
-    }
+    //   // Compute the distance to the target based on height and power to move forward/backward
+    //   if(Math.abs(verticalOffset) > 0){
+    //   targetDistance = targetHeight / Math.tan(verticalOffset*Math.PI/180);
+    //   }
+    //   else{targetDistance = optimalDistance;}
+    //   SmartDashboard.putNumber("targetDistance", targetDistance);
+    //   if(Math.abs(optimalDistance - targetDistance) >= 6 ){
+    //     if(targetDistance > optimalDistance) {
+    //       newX = minPowerFloor;
+    //     }
+    //     else if(targetDistance < optimalDistance){
+    //       newX = -minPowerFloor;
+    //     }
+    //   }
+    //   else {newX = 0.0;}
+    //   SmartDashboard.putNumber("newX", newX);
+    //   //Distance tracking by target area - decided too likely to be wrong
+    //   // if (targetArea < 0.45) {
+    //   //   newY = minPowerFloor*1.15;
+    //   // } else if (targetArea > 0.65) {
+    //   //   newY = -minPowerFloor*1.15;
+    //   // }
+    //   m_robotDrive.driveCartesian(newY, newX, newZ, 0.0);
+    // }
   }
 
 }
