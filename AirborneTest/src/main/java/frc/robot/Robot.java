3efc93246fd9e2e8da5c011cc00767bd5c9b3100
FRCTeam1518Student1 <Raider.Robotics@palmaccsd.org>
2020-01-25 11:50:45 -0500

    added encoder to thrower motor

--- AirborneTest/src/main/java/frc/robot/Robot.java
+++ AirborneTest/src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -28,12 +29,14 @@ import edu.wpi.first.wpilibj.interfaces.Gyro;
  */
 public class Robot extends TimedRobot {
   private static final int kFrontLeftChannel = 5;
-  private static final int kRearLeftChannel = 8;
+  private static final int kRearLeftChannel = 6;
   private static final int kFrontRightChannel = 10;
   private static final int kRearRightChannel = 9;
   private static final int kThrowerMotor = 4;
 
   private static final int kJoystickChannel = 0;
+
+  private CANEncoder m_encoder;
 
   private MecanumDrive m_robotDrive;
   private Joystick m_stick;
@@ -42,8 +45,8 @@ public class Robot extends TimedRobot {
   public static boolean DriveByStick = false;
   public JoystickButton btnDriveMode;
 
-  private static final double targetHeight = 18;  //Inches
-  private static final double optimalDistance = 84; //Inches
+  private static final double targetHeight = 18; // Inches
+  private static final double optimalDistance = 84; // Inches
 
   public static Gyro rioGyro = new ADXRS450_Gyro();
 
@@ -60,6 +63,10 @@ public class Robot extends TimedRobot {
 
     throwerMotor1 = new com.revrobotics.CANSparkMax(kThrowerMotor, MotorType.kBrushless);
 
+    throwerMotor1.restoreFactoryDefaults();
+
+    m_encoder = throwerMotor1.getEncoder();
+
     rioGyro.calibrate();
     rioGyro.reset();
 
@@ -74,7 +81,7 @@ public class Robot extends TimedRobot {
 
     m_stick = new Joystick(kJoystickChannel);
 
-    lime.init("limelight-front");
+    lime.init("limelight");
   }
 
   @Override
@@ -84,58 +91,57 @@ public class Robot extends TimedRobot {
     throwerMotor1.set(m_stick.getThrottle());
     SmartDashboard.putNumber("Thrower Throttle", m_stick.getThrottle());
     // Use button 11 to change drive mode (joystick or LimeLight)
-    //btnDriveMode.whenPressed(new ChangeDrive());
+    // btnDriveMode.whenPressed(new ChangeDrive());
 
     // Use the joystick X axis for lateral movement, Y axis for forward
     // movement, and Z axis for rotation.
-    SmartDashboard.putNumber("Gyro Angle", rioGyro.getAngle());
-    // if(DriveByStick){
-      m_robotDrive.driveCartesian(Math.pow(m_stick.getX(), 3)*.75, Math.pow(-m_stick.getY(), 3)*1, Math.pow(m_stick.getZ(), 3)*.5, 0.0);
-    // }
-    // else{
-    //   Double horizontalOffset = lime.getTargetOffsetHorizontal();
-    //   Double verticalOffset = lime.getTargetOffsetVertical();
-    //   Double targetArea = lime.getTargetArea();
-  
-    //   Double minPowerFloor = 0.25;
-    //   Double newX = 0.0;
-    //   Double newY = 0.0;
-    //   Double newZ = 0.0;
-    //   Double targetDistance = optimalDistance;
+    // SmartDashboard.putNumber("Gyro Angle", rioGyro.getAngle());
 
-    //   // Compute the left or right power to turn the robot to face the target
-    //   if (Math.abs(horizontalOffset) > 8.0) {
-    //     // newZ = minPowerFloor * (int)Math.signum(horizontalOffset);
-    //     newZ = minPowerFloor* (int)Math.signum(horizontalOffset) + Math.sin(horizontalOffset*Math.PI/180);
-    //   }
-    //   else {
-    //     newZ = 0.0;
-    //   }
+    SmartDashboard.putNumber("Thrower Velocity", m_encoder.getVelocity());
 
-    //   // Compute the distance to the target based on height and power to move forward/backward
-    //   if(Math.abs(verticalOffset) > 0){
-    //   targetDistance = targetHeight / Math.tan(verticalOffset*Math.PI/180);
-    //   }
-    //   else{targetDistance = optimalDistance;}
-    //   SmartDashboard.putNumber("targetDistance", targetDistance);
-    //   if(Math.abs(optimalDistance - targetDistance) >= 6 ){
-    //     if(targetDistance > optimalDistance) {
-    //       newX = minPowerFloor;
-    //     }
-    //     else if(targetDistance < optimalDistance){
-    //       newX = -minPowerFloor;
-    //     }
-    //   }
-    //   else {newX = 0.0;}
-    //   SmartDashboard.putNumber("newX", newX);
-    //   //Distance tracking by target area - decided too likely to be wrong
-    //   // if (targetArea < 0.45) {
-    //   //   newY = minPowerFloor*1.15;
-    //   // } else if (targetArea > 0.65) {
-    //   //   newY = -minPowerFloor*1.15;
-    //   // }
-    //   m_robotDrive.driveCartesian(newY, newX, newZ, 0.0);
-    // }
+    if (true || DriveByStick) {
+      m_robotDrive.driveCartesian(Math.pow(m_stick.getX(), 3) * .75, Math.pow(-m_stick.getY(), 3) * 1,
+          Math.pow(m_stick.getZ(), 3) * .5, 0.0);
+    } else {
+      Double horizontalOffset = lime.getTargetOffsetHorizontal();
+      Double verticalOffset = lime.getTargetOffsetVertical();
+      Double targetArea = lime.getTargetArea();
+
+      Double minPowerFloor = 0.25;
+      Double newX = 0.0;
+      Double newY = 0.0;
+      Double newZ = 0.0;
+      Double targetDistance = optimalDistance;
+
+      // Compute the left or right power to turn the robot to face the target
+      if (Math.abs(horizontalOffset) > 8.0) {
+        // newZ = minPowerFloor * (int)Math.signum(horizontalOffset);
+        newZ = minPowerFloor * (int) Math.signum(horizontalOffset) + Math.sin(horizontalOffset * Math.PI / 180);
+      } else {
+        newZ = 0.0;
+      }
+
+      // Compute the distance to the target based on height and power to move
+      // forward/backward
+      if (Math.abs(verticalOffset) > 0) {
+        targetDistance = targetHeight / Math.tan(verticalOffset * Math.PI / 180);
+      } else {
+        targetDistance = optimalDistance;
+      }
+      SmartDashboard.putNumber("targetDistance", targetDistance);
+      if (Math.abs(optimalDistance - targetDistance) >= 6) {
+        if (targetDistance > optimalDistance) {
+          newX = minPowerFloor;
+        } else if (targetDistance < optimalDistance) {
+          newX = -minPowerFloor;
+        }
+      } else {
+        newX = 0.0;
+      }
+      SmartDashboard.putNumber("newX", newX);
+      SmartDashboard.putNumber("newZ", newZ);
+      m_robotDrive.driveCartesian(newY, newX, newZ, 0.0);
+    }
   }
 
 }
